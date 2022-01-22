b7c9e57d0c6e7f1641d179189b12fec056c937ad
Pedro Furtado <pedro.nazare@pdcase.com.br>
2022-01-22 00:43:21 -0300

    feat:  método post criado

--- src/main/java/spring/api/rest/controller/IndexController.java
+++ src/main/java/spring/api/rest/controller/IndexController.java
@@ -12,33 +12,30 @@ import spring.api.rest.repository.UsuarioRepository;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/usuario")
+@RequestMapping(value = "/api/v1")
 public class IndexController {
 
     @Autowired
     private UsuarioRepository repository;
 
-    @GetMapping(value = "/{id}", produces = "application/json")
+    @GetMapping(value = "/usuario/{id}", produces = "application/json")
     public ResponseEntity<Usuario> init(@PathVariable(value = "id") Long id) throws Exception {
         Usuario usuario = repository.findById(id).orElseThrow(() -> new Exception("Usuario não encontrado"));
         return ResponseEntity.ok(usuario);
     }
 
-    @GetMapping(value = "/", produces = "application/json")
+    @GetMapping(value = "/usuarios", produces = "application/json")
     public ResponseEntity<List<Usuario>> listaUsuarios(){
         List<Usuario> usuarios = repository.findAll();
         return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
     }
 
-    @PostMapping(value = "/")
+    @PostMapping(value = "/", produces = "application/json")
     public ResponseEntity<Usuario> criar(@RequestBody Usuario request){
 
-        RequestUsuario usuario = new RequestUsuario();
-        usuario.setLogin("oseais@pdcase.com.br");
-        usuario.setNome("Oeias");
-        usuario.setSenha("123456");
+        Usuario usuario = repository.save(request);
 
-        return  null;
+        return new ResponseEntity(usuario, HttpStatus.CREATED);
     }
 
 }
